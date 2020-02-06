9fd76b83895ff7a1bbb6b8189de73156d35eb6d4
Uma Manandhar <um_2003_8@hotmail.com>
2020-02-06 02:44:03 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,8 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
 
    def change
-    add_column :grade, :integer, :birthdate, :string
+    add_column :grade, :integer 
+    add_column :birthdate, :string
    end
 
 end
